2cdbee740ebcefc9bce9c86959715ab3d8bc0bea
Kenneth <kensonoda1124@gmail.com>
2026-03-27 12:53:05 +1100

    Added totalmmin + helper functions

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ const addBtn = document.querySelector(".add-btn");
 const shifts = [];
 
 
-
 addBtn.addEventListener("click", (e) => {
     e.preventDefault();
     const formData = Object.fromEntries(new FormData(form));
@@ -14,12 +13,34 @@ addBtn.addEventListener("click", (e) => {
         date: formData.date,
         startTime: formData.startTime,
         endTime: formData.endTime,
+        totalMin: getTotalHours(formData.startTime, formData.endTime),
         break: formData.break,
         workPlace: formData.workPlace
     }
-    console.log(shift)
     shifts.push(newShift)
+    console.log(newShift)
+    console.log(convertHour(newShift.totalMin))
 })
 
+
+function getTotalHours(start, end) {
+
+    const [startHour, startMin] = start.split(":").map(Number);
+    const [endHour, endMin] = end.split(":").map(Number)
+
+    return (endHour * 60 + endMin) - (startHour * 60 + startMin)
+}
+
+function convertHour(totalmins) {
+    const hours = Math.floor(totalmins / 60);
+    const mins = totalmins % 60;
+
+    return `${hours}h ${mins}m`;
+}
+
+function getDayName(date) {
+    const days = ["monday", 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+    return days[(new Date(date).getDay())];
+}
 
 
